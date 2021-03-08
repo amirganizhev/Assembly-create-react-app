fbd335a3b4cc966ea9f2a3d00c957fd52003485b
Amir <ganizhev.amir@mail.ru>
2021-03-08 21:00:16 +0300

    Добавил функции и обработчики событий на компоненты

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 import './button.css'
 
 export default class Form extends Component {
+
+	buttonClick = () => {
+		console.log('button');
+	}
+
 	render() {
 		return (
-			<button>start</button>
+			<button onClick={ this.buttonClick }>start</button>
 		)
 	}
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 import './header.css'
 
 export default class Header extends Component {
+
+	headerClick = () => {
+		console.log('header');
+	}
+
 	render() {
 		return (
-			<h1>Hello world</h1>
+			<h1 onClick={ this.headerClick }>Hello world</h1>
 		)
 	}
 }
